760dc48663ab6b014659e0cb2cc36a53bc38caa5
khazaaaar <zloiegor@mail.ru>
2022-12-26 10:53:00 +0200

    1

---
+++ src/emums.ts
@@ -0,0 +1,6 @@
+export enum Role {
+    SystemAdministrator = "System administrator",
+    CompetitionAdministrator = "Competition administrator",
+    Participant = "Participant",
+    TasksAdministrator = "Tasks administrator",
+}
